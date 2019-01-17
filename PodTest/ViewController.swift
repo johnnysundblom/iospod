916dbec5d4803d7270d8636f714b37c037aa327e
Johnny Sundblom <johnnysundblom@Johnnys-MacBook-Pro.local>
2019-01-17 14:59:02 +0100

    Initial commit

---
+++ PodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by Johnny Sundblom on 2019-01-17.
+//  Copyright © 2019 Johnny Sundblom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
